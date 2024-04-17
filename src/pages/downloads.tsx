c795cf87faf4d447a305fb3c8aee09aa80ac3ac8
Microgramx <geral@webproject.pt>
2024-04-17 20:11:13 +0200

    Update downloads.tsx

--- src/pages/downloads.tsx
+++ src/pages/downloads.tsx
@@ -1,81 +1,55 @@
 import { useState, useEffect } from "react";
 import styles from "@/styles/Settings.module.scss";
-
 const LoginPage = () => {
   const [deferredPrompt, setDeferredPrompt] = useState<any>();
-
   useEffect(() => {
-    const handleBeforeInstallPrompt = (e: Event) => {
+    window.addEventListener("beforeinstallprompt", (e) => {
       e.preventDefault();
       setDeferredPrompt(e);
-    };
-
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
-
-    return () => {
-      window.removeEventListener(
-        "beforeinstallprompt",
-        handleBeforeInstallPrompt,
-      );
-    };
+    });
   }, []);
-
   const handleDownload = async () => {
-    if (deferredPrompt) {
-      try {
-        deferredPrompt.prompt();
-        const { outcome } = await deferredPrompt.userChoice;
-        if (outcome === "accepted") {
-          setDeferredPrompt(null);
-        }
-      } catch (error) {
-        console.error("Erro ao iniciar o download:", error);
+    if (deferredPrompt !== null && deferredPrompt !== undefined) {
+      deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === "accepted") {
+        setDeferredPrompt(null);
       }
-    } else {
-      alert("O seu navegador não suporta a instalação de PWAs.");
     }
   };
-
   return (
     <div className={`${styles.settingsPage} ${styles.authPage}`}>
       <div className={styles.logo}>
         <img
-          src="/images/logolmg.png"
+          src="/images/logo.svg"
           alt="logo"
           data-tooltip-id="tooltip"
           data-tooltip-content="Rive"
         />
-        <p>WarezAPP</p>
+        <p>Download WarezAPP</p>
       </div>
       <div className={styles.settings}>
         <h1>Downloads</h1>
         <div className={styles.group2}>
-          <h1>App Beta v2</h1>
+          <h1>Warez APP</h1>
           <p>
-            Isso instalará o aplicativo para todos os dispositivos ocupando
-            pouca memória e dados móveis
+            Corrigimos alguns bugs do download do App em alguns navegadores
           </p>
           <p>
-            Baixe usando qualquer Navegador, utilize AD-Block para tentar
-            remover ou minimizar a publicidade dos Players
+            Experimente fazer o download do APP, se o download nao iniciar desligue a traducao automatica e tente novamente.
+            Se nao inciar o download tente utilizar outro navegador de sua preferencia.
+            Ainda estamos corrigindo alguns bugs na versao IOs, para utilizadores de modelos IOs, pedimos a sua compreesao e paciencia
           </p>
-          <p>
-            Se o download não iniciar ou o botão não funcionar, desligue a
-            tradução automática, atualize e tente novamente!
-          </p>
-          <p>
-            * Por favor, note que o App ainda está em fase de teste, bugs e
-            erros serão corrigidos com o tempo.
-          </p>
+          <p></p>
           {/* <p>To download movies/tv shows, go to it's watch page, and use extensions like FetchV</p> */}
-          <button
+          <h4
             className={styles.downloadButton}
             onClick={handleDownload}
             data-tooltip-id="tooltip"
             data-tooltip-content="Download PWA"
           >
             Download
-          </button>
+          </h4>
         </div>
       </div>
     </div>
