92219bc4272137548b9a7ea39e19623e046e69cb
Microgramsss <118573098+Microgramx@users.noreply.github.com>
2024-05-29 15:45:31 +0200

    Fix Ftwatch

--- src/pages/ftwatch.tsx
+++ src/pages/ftwatch.tsx
@@ -73,13 +73,13 @@ const FTWatch = () => {
         {/* Adicione mais opções para os outros servidores, conforme necessário */}
       </select>
       <div className={styles.videoContainer}>
-      <iframe
+        <iframe
           key={videoKey} // Use uma chave única para forçar a atualização do iframe
-          scrolling="no"
           src={getVideoUrl()}
           className={styles.iframe}
           allowFullScreen
           allow="autoplay"
+          loading="lazy" // Adiciona o atributo lazy loading
           sandbox="allow-same-origin allow-scripts"
         ></iframe>
       </div>
--- src/pages/futebol.tsx
+++ src/pages/futebol.tsx
@@ -4,15 +4,15 @@ import styles from "../components/Futebol/style.module.scss";
 const ContentGrid: React.FC = () => {
   // Array de conteúdos fictícios
   const contents = [
-    { id: 1, link: "/ftwatch?type=tv&id=1&server=STV1", image: "/images/futebol/artboard-1.png" },
-    { id: 2, link: "/ftwatch?type=tv&id=2&server=STV2", image: "/images/futebol/artboard-2.png" },
-    { id: 3, link: "/ftwatch?type=tv&id=3&server=STV3", image: "/images/futebol/artboard-3.png" },
-    { id: 4, link: "/ftwatch?type=tv&id=4&server=STV4", image: "/images/futebol/artboard-4.png" },
-    { id: 5, link: "/ftwatch?type=tv&id=5&server=STV5", image: "/images/futebol/artboard-5.png" },
-    { id: 6, link: "/ftwatch?type=tv&id=6&server=ELS1", image: "/images/futebol/artboard-6.png" },
-    { id: 7, link: "/ftwatch?type=tv&id=7&server=ELS2", image: "/images/futebol/artboard-7.png" },
-    { id: 8, link: "/ftwatch?type=tv&id=8&server=ELS3", image: "/images/futebol/artboard-8.png" },
-    { id: 9, link: "/ftwatch?type=tv&id=9&server=STV4", image: "/images/futebol/artboard-10.png" },
+    { id: 1, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=1&server=STV1", image: "/images/futebol/artboard-1.png" },
+    { id: 2, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=2&server=STV2", image: "/images/futebol/artboard-2.png" },
+    { id: 3, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=3&server=STV3", image: "/images/futebol/artboard-3.png" },
+    { id: 4, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=4&server=STV4", image: "/images/futebol/artboard-4.png" },
+    { id: 5, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=5&server=STV5", image: "/images/futebol/artboard-5.png" },
+    { id: 6, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=6&server=ELS1", image: "/images/futebol/artboard-6.png" },
+    { id: 7, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=7&server=ELS2", image: "/images/futebol/artboard-7.png" },
+    { id: 8, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=8&server=ELS3", image: "/images/futebol/artboard-8.png" },
+    { id: 9, link: "https://wztuga.netlify.app/ftwatch?type=tv&id=9&server=STV4", image: "/images/futebol/artboard-10.png" },
   ];
 
   return (
